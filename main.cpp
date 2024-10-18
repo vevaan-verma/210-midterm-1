fbfc64e9bb06f849e538b77caeda145be08edb20
Vevaan <vevaan.s.verma@gmail.com>
2024-10-17 18:54:01 -0700

    Document delete & push methods

--- main.cpp
+++ main.cpp
@@ -28,6 +28,9 @@ private:
 public:
 	DoublyLinkedList() { head = nullptr; tail = nullptr; }
 
+	// insert_after() inserts a new node after a specified position
+	// arguments: int value - the value to insert, int position - the position to insert the value after
+	// returns: void
 	void insert_after(int value, int position) {
 
 		// make sure position is actually a valid position (it must be >= 0)
@@ -75,6 +78,9 @@ public:
 
 	}
 
+	// delete_val() deletes the first occurrence of a value in the list
+	// arguments: int value - the value to delete
+	// returns: void
 	void delete_val(int value) {
 
 		if (!head) return; // make sure the list isn't empty
@@ -100,90 +106,99 @@ public:
 
 	}
 
+	// delete_pos() deletes a node at a specified position
+	// arguments: int position - the position to delete the node at
+	// returns: void
 	void delete_pos(int pos) {
 
-		if (!head) {
+		if (!head) { // if the list is empty, print an error message and return
 
 			cout << "List is empty." << endl;
 			return;
 
 		}
 
-		if (pos == 1) {
+		if (pos == 1) { // if the position is 1, you can just pop the front of the list and return
 
 			pop_front();
 			return;
 
 		}
 
-		Node* temp = head;
+		Node* temp = head; // create a temporary node to traverse the list and initialize it to the head
 
-		for (int i = 1; i < pos; i++) {
+		for (int i = 1; i < pos; i++) { // traverse the list until the position is reached
 
-			if (!temp) {
+			if (!temp) { // if the end of the list is reached before the position, print an error message and return
 
 				cout << "Position doesn't exist." << endl;
 				return;
 
 			} else {
 
-				temp = temp->next;
+				temp = temp->next; // move to the next node
 
 			}
 		}
 
-		if (!temp) {
+		if (!temp) { // if the position is invalid, print an error message and return
 
 			cout << "Position doesn't exist." << endl;
 			return;
 
 		}
 
-		if (!temp->next) {
+		if (!temp->next) { // if the node to delete is the tail, you can just pop the back of the list and return
 
 			pop_back();
 			return;
 
 		}
 
-		Node* tempPrev = temp->prev;
-		tempPrev->next = temp->next;
-		temp->next->prev = tempPrev;
+		Node* tempPrev = temp->prev; // create a temporary node to store the previous node of the node to delete
+		tempPrev->next = temp->next; // set the next pointer of the previous node to the next node of the node to delete
+		temp->next->prev = tempPrev; // set the previous pointer of the next node of the node to delete to the stored previous node
 
-		delete temp;
+		// now all pointers have been rerouted correctly, so the current node can be deleted
+
+		delete temp; // delete the node
 
 	}
 
 	void push_back(int v) {
 
-		Node* newNode = new Node(v);
+		Node* newNode = new Node(v); // create a new node with the passed value
 
-		if (!tail) {
+		if (!tail) { // if the list is empty, set the head and tail to the new node
 
 			head = tail = newNode;
 
 		} else {
 
-			tail->next = newNode;
-			newNode->prev = tail;
-			tail = newNode;
+			tail->next = newNode; // set the current tail's next pointer to the new node
+			newNode->prev = tail; // set the new node's previous pointer to the current tail
+			tail = newNode; // set the tail to the new node
+
+			// the code above adds the new node to the end of the list and moves the tail to this node to reflect this addition
 
 		}
 	}
 
 	void push_front(int v) {
 
-		Node* newNode = new Node(v);
+		Node* newNode = new Node(v); // create a new node with the passed value
 
-		if (!head) {
+		if (!head) { // if the list is empty, set the head and tail to the new node
 
 			head = tail = newNode;
 
 		} else {
 
-			newNode->next = head;
-			head->prev = newNode;
-			head = newNode;
+			newNode->next = head; // set the new node's next pointer to the current head
+			head->prev = newNode; // set the current head's previous pointer to the new node
+			head = newNode; // set the head to the new node
+
+			// the code above adds the new node to the front of the list and moves the head to this node to reflect this addition
 
 		}
 	}
